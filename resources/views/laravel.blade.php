493d5631ce08a68fd42b6df33f840175a89fedf6
Ruma <matteoh8@hotmail.it>
2023-08-20 22:03:45 +0200

    FE solo Laravel

--- resources/views/laravel.blade.php
+++ resources/views/laravel.blade.php
@@ -1,17 +1,21 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'My Site') }}</title>
-</head>
+    <!doctype html>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'My Site') }}</title>
+        <link rel="icon" href="{{ asset('roma.ico') }}" type="image/x-icon">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
 <body>
-    <div id="container py-2">
-        <h1 class="text-center mt-3">Laravel</h1>
-    </div>
+    <header>
+        <h1 id="page-title">Solo Laravel - No FE Bundler</h1>
+    </header>
+    <main>
+        <img src="{{ asset('img/pizza-small.png') }}"
+            alt="img/pizza-small.png">
+    </main>
 </body>
 </html>
-
-{{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
