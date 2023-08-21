e3467007ec81bf2ea555e15e6df04f15065a12fa
Ruma303 <matteoh8@hotmail.it>
2023-08-21 19:38:12 +0200

    fino a index()

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.master');
 });
+//Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+    Route::get('/users', [App\Http\Controllers\UserController::class, 'index']);
+    Route::get('/users/create', [App\Http\Controllers\UserController::class, 'create']);
+    Route::post('/users', [App\Http\Controllers\UserController::class, 'store']);
+    Route::get('/users/{user}', [App\Http\Controllers\UserController::class, 'show']);
+    Route::get('/users/{user}/edit', [App\Http\Controllers\UserController::class, 'edit']);
+    Route::put('/users/{user}', [App\Http\Controllers\UserController::class, 'update']);
+    Route::delete('/users/{user}', [App\Http\Controllers\UserController::class, 'destroy']);
+
+    //Route::resource('/users', App\Http\Controllers\UserController::class);
