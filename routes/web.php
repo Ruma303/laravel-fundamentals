c8fc990d4750045c47c116b7de4cb6aaf702757c
Ruma303 <matteoh8@hotmail.it>
2023-08-25 19:59:21 +0200

    tests 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json(['message' => 'Connessione al database avvenuta con successo.'], 200);
+    } catch (\Exception $e) {
+        return response()->json(['message' => 'Connessione al database non riuscita.', 'exception' =>
+            $e->getMessage()], 500);
+    }
 });
