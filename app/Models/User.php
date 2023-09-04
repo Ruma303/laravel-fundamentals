634aa7f39ecc1f9b582f4b5ff23d263fcc656e7a
Ruma303 <matteoh8@hotmail.it>
2023-09-04 15:36:11 +0200

    aggiornamento fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,9 +11,14 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
     protected $fillable = [
         'name',
         'email',
-        'password',
     ];
+
+    protected $guarded = [
+        'password'
+    ];
+
 }
