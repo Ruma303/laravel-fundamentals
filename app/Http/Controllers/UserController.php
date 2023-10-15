75cb0f93a81382e095e79a1db7f5baac4737fba9
Ruma303 <matteoh8@hotmail.it>
2023-10-15 11:19:46 +0200

    Tolte validazioni

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,26 +7,6 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    private $inputValidations = [
-        'name'      => ['required', 'min:4', 'max:100'],
-        'email'     => ['required', 'unique:users,email', 'min:4', 'max:100'],
-        'password'  => ['required', 'min:4', 'max:100']
-    ];
-
-    private $validationsMessages = [
-        'name.required'     =>  'Il nome è richiesto',
-        'name.min'          =>  'Minimo 4 caratteri',
-        'name.max'          =>  'Massimo 100 caratteri',
-
-        'email.required'    =>  'L\'email è richiesta',
-        'email.unique'      =>  'L\'email dev\'essere unica',
-        'email.min'         =>  'Minimo 4 caratteri',
-        'email.max'         =>  'Massimo 100 caratteri',
-
-        'password.required' =>  'La password è richiesta',
-        'password.min'      =>  'Minimo 4 caratteri',
-        'password.max'      =>  'Massimo 100 caratteri',
-    ];
 
     public function index()
     {
@@ -127,67 +107,6 @@ class UserController extends Controller
         ]);
     } */
 
-    //% Validazione
-    /* public function store(Request $request)
-    { */
-        //* Validazione dati in input
-        /* $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email',
-            'password' => 'required|string'
-        ]); */
-        /* $request->validate([
-            'name' => 'required|min:4|max:255',
-            'email' => 'required|unique:users|min:4|max:100',
-            'password' => 'required|min:4|max:100'
-        ]);
-         */
-        /* $request->validate([
-            'name' => ['required', 'string'],
-            'email' => ['required', 'email'],
-            'password' => ['required', 'string']
-        ]); */
-        /* $request->validate([
-            'name'      => ['required', 'min:4', 'max:100'],
-            'email'     => ['required', 'unique:users,email', 'min:4', 'max:100'],
-            'password'  => ['required', 'min:4', 'max:100']
-        ], [
-            'name.required'     =>  'Il nome è richiesto',
-            'name.min'          =>  'Minimo 4 caratteri',
-            'name.max'          =>  'Massimo 100 caratteri',
-
-            'email.required'    =>  'L\'email è richiesta',
-            'email.unique'      =>  'L\'email dev\'essere unica',
-            'email.min'         =>  'Minimo 4 caratteri',
-            'email.max'         =>  'Massimo 100 caratteri',
-
-            'password.required' =>  'La password è richiesta',
-            'password.min'      =>  'Minimo 4 caratteri',
-            'password.max'      =>  'Massimo 100 caratteri',
-        ]); */
-        //dd('Validazione soddisfatta');
-
-    public function store(Request $request)
-    {
-        //* Validazioni
-        $request->validate(
-            $this->inputValidations,
-            $this->validationsMessages
-        );
-
-        //* Creazione del nuovo utente
-        $user = User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password')),
-        ]);
-
-        //* Reindirizzamento verso l'index degli users
-        return redirect('/users')->with([
-            'created' => "User {$user->name} has been created."
-        ]);
-    }
-
 
     public function show(User $user)
     {
@@ -243,29 +162,6 @@ class UserController extends Controller
         //* Reindirizzamento alla vista con i dati aggiornati
         return redirect()->route('users.show', compact('user'));
     } */
-
-
-    //% Aggiornare record con fill()
-    public function update(Request $request, User $user)
-    {
-        //* Validazioni
-        $request->validate(
-            $this->inputValidations,
-            $this->validationsMessages
-        );
-
-        //* Aggiornamento dell'utente usando fill()
-        $user->fill([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password')),
-        ]);
-        $user->save();
-
-        //* Reindirizzamento alla vista dettagli con i dati aggiornati
-        return redirect()->route('users.show', compact('user'));
-    }
-
 
     public function destroy(User $user)
     {
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,64 +2,28 @@
 @section('title', 'Create View')
 @section('content')
     <h1>Create Page</h1>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            Ci sono alcuni errori durante il tuo input.</div>
-        @dump($errors)
-    @endif
+
+
     <form method="POST" action="{{ route('users.store') }}">
         @csrf
         <div class="form-group row">
             <label for="name" class="col-sm-2 col-form-label">Name</label>
-            {{-- {{ dd($errors) }} --}}
 
-
-            {{-- @foreach ($errors->all() as $error)
-                <p class="text-danger">{{ $error }}</p>
-            @endforeach --}}
 
             <div class="col-sm-10">
-                <input type="text" class="form-control @error('name')
-                border border-3 border-danger rounded @enderror" id="name" placeholder="Your name"
-                value="{{ old('name') }}"
-                name="name">
-
-                @error('name')
-                    <ul class="alert alert-danger">
-                        @foreach ($errors->get('name') as $error)
-                            <li class="text-danger">*{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @enderror
-
-                {{-- <input type="text" class="form-control" id="name" placeholder="Your name" name="name">
-                @error('name')
-                    <p class="alert alert-danger">{{ $message }}</p>
-                @enderror --}}
+                <input type="text"
+                    class="form-control @error('name')
+                border border-3 border-danger rounded @enderror"
+                    id="name" placeholder="Your name" value="{{ old('name') }}" name="name">
 
             </div>
 
             <label for="email" class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-10">
 
-                {{-- <input type="email" class="form-control" id="email" placeholder="Your mail" name="email">
-                @error('email')
-                    <p class="alert alert-danger">{{ $message }}</p>
-                @enderror --}}
-
                 <input type="email" id="email" placeholder="Your mail" name="email"
                     class="form-control {{ $errors->has('email') ? 'border border-3 border-danger rounded' : '' }}"
                     value="{{ old('email') }}">
-                {{-- @error('email')
-                    <p class="alert alert-danger">{{ $message }}</p>
-                @enderror --}}
-                @error('email')
-                    <ul class="alert alert-danger">
-                        @foreach ($errors->get('email') as $error)
-                            <li class="text-danger">*{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @enderror
 
             </div>
         </div>
@@ -68,24 +32,12 @@
             <label for="password" class="col-sm-2 col-form-label">Password</label>
             <div class="col-sm-10">
 
-                {{-- <input type="password" class="form-control" id="password" placeholder="Your password" name="password">
-                @error('password')
-                    <p class="alert alert-danger">{{ $message }}</p>
-                @enderror --}}
 
-                <input type="text" class="form-control @error('password')
-                    border border-3 border-danger rounded @enderror" id="password" name="password"
+                <input type="text"
+                    class="form-control @error('password')
+                    border border-3 border-danger rounded @enderror"
+                    id="password" name="password"
                     placeholder="{{ $errors->has('password') ? 'Please, provide a password.' : 'Your password' }}">
-                {{-- @error('password')
-                    <p class="alert alert-danger">{{ $message }}</p>
-                @enderror --}}
-                @error('password')
-                    <ul class="alert alert-danger">
-                        @foreach ($errors->get('password') as $error)
-                            <li class="text-danger">*{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @enderror
 
             </div>
         </div>
