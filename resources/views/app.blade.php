e53ed7264720dac727946459ec5e19a24d844ece
Ruma303 <matteoh8@hotmail.it>
2024-07-14 12:26:25 +0200

    bootstrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -65,6 +65,11 @@
     {{-- {{ $x, $y }}; --}}
 
 
+    {{-- , @once --}}
+
+    {{-- @once
+        <script src="https://example.com/script.js"></script>
+    @endonce --}}
 
 
     {{-- , @dump, @dd --}}
@@ -155,9 +160,6 @@
     </ul> --}}
 
 
-    {{-- , @each --}}
-    {{-- @each('each', $items, 'item', 'empty') --}}
-
 
     {{-- , @forelese, @empty --}}
     {{-- @forelse($items as $item)
@@ -200,7 +202,6 @@
 
 
     {{-- , @class, @style --}}
-
     {{-- @php
         $isActive = false;
         $hasError = true;
@@ -221,7 +222,6 @@
 
 
     {{-- , @checked(), @selected(), @disabled(), @readonly(), @required() --}}
-
     {{-- <label>Ci sono le scarpe nell'array $items?</label>
     <input type="radio" @checked(in_array("Scarpe", $items))>
 
@@ -255,9 +255,15 @@
 
     {{-- % Ereditarietà --}}
 
+
     {{-- , @extends --}}
 
-    {{-- @extends('layouts.base') --}}
+    @extends('layouts.base')
+
+
+    {{-- , @each --}}
+    {{-- @each('each', $items, 'item', 'empty') --}}
+
 
     {{-- , @section --}}
     {{-- @section('title', 'Blade Tutorial') --}}
@@ -266,18 +272,7 @@
         <h3>Siamo in app.blade.php</h3>
     @endsection
     --}}
-
-    {{-- @section('bootstrap')
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous" defer>
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-            integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous" defer>
-        </script>
-    @endsection
-    --}}
+    
 
     {{-- , @show Footer --}}
 
