f2a35144a363931a56249abddebf5d2c9ab028b9
Ruma303 <matteoh8@hotmail.it>
2024-07-17 10:35:43 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('layouts.master');
-});
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
