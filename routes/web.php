4be65bdd568c52b3fc3fd6d43d3ed066a923191c
Ruma303 <matteoh8@hotmail.it>
2024-07-17 09:55:37 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -2,37 +2,26 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('layouts.master');
 });
 //Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [HomeController::class, 'index'])->name('home');'
 
-/* Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
-Route::post('/users', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
-Route::get('/users/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('users.show');
-Route::get('/users/{user}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy'); */
+/* Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy'); */
 
-    Route::get('/users/trash', [App\Http\Controllers\UserController::class, 'trash'])
-        ->name('user.trash');
-    Route::get('/users/{id}/restore', [App\Http\Controllers\UserController::class, 'restore'])
-        ->name('user.restore');
-    Route::delete('/users/{id}/force-delete', [App\Http\Controllers\UserController::class, 'forceDelete'])
-        ->name('users.forceDelete');
-    Route::resource('/users', App\Http\Controllers\UserController::class);
+    Route::resource('/users', UserController::class);
+
+    Route::get('/users/trash', [UserController::class, 'trash'])->name('user.trash');
+    Route::get('/users/{id}/restore', [UserController::class, 'restore'])->name('user.restore');
+    Route::delete('/users/{id}/force-delete', [UserController::class, 'forceDelete'])->name('users.forceDelete');
