cec70ff1afc7b52bbae167d85b837d34fab85f1d
Ruma303 <matteoh8@hotmail.it>
2024-07-20 18:56:56 +0200

    .

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,9 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //protected $table = 'libri';
-
     use HasFactory;
-    const CREATED_AT = 'creato_il';
-    const UPDATED_AT = 'aggiornato_il';
+
+    //protected $table = 'libri';
+    //protected $primaryKey = 'book_id';
+    //public $incrementing = false;
+
+    //public $timestamps = false;
+    //protected $dateFormat = 'U';
+
+    /* protected $attributes = [
+        'title' => 'default title',
+        'description' => 'default description',
+        'genres' => '[{"genre_id":1,"name":"Fantasy"},{"genre_id":2,"name":"Horror"}]',
+    ]; */
+
+    //const CREATED_AT = 'creato_il';
+    //const UPDATED_AT = 'aggiornato_il';
 }
